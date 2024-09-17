31d4f542579e5cedc80ea74ac3aed3bca6508188
jcardogo <jcardogo@gmail.com>
2024-09-17 15:54:53 -0400

    Fisrt commit from my ububtu server from HomeLab

---
+++ Basic_Python_Syntax_introduction.py
@@ -0,0 +1,13 @@
+#data types
+
+#def to_celsius(x: any) -> any
+#		‘’’Convert Farenheit to Celsius’’’
+#		return (x-32) * 5/9
+#
+#	to_celsius(75)
+
+
+def to_celsius(x):
+    '''Convert Fahrenheit to Celsius'''
+    return (x-32) * 5/9
+print(to_celsius(75))
