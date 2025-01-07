348cc671fe8d8c96cb30e57fa6b703c4e4309c69
Alejandro Cardoso <jcardogo@gmail.com>
2025-01-06 21:14:11 -0500

    Update excemption_aproach.py
    port check on exceptions January 6th

--- Automating_Real-World_Tasks_with_Python/logging_module/excemption_aproach.py
+++ Automating_Real-World_Tasks_with_Python/logging_module/excemption_aproach.py
@@ -1,5 +1,36 @@
+#!/usr/bin/env python3
 
+"""
+#Basic try/except code 
 try:
   print(x)
 except NameError:
   print("Variable x is not defined")
+"""
+
+"""
+#More advanced try/except code adding extra error
+try:
+  f = open("etc/hosts", "w+")
+  f.write("success!")
+except FileNotFoundError:
+  print("Data file not found")
+except Exception as ex:
+  print("Error appending to file: " + str(ex))
+else:
+  f.close()
+"""
+
+"""
+x = "hello"
+if not isinstance(x, int):
+  raise TypeError("Only integers are allowed")
+"""
+
+def start_server(port):
+  if not isinstance(x, int):
+    raise TypeError("Port number must be an integer")
+  elif port < 1024 or port > 65535:
+    raise ValueError ("Port number is invalid")
+port = 1023
+
