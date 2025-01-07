2dc40408fc3a2c18e1ff15338728d5cab327f43f
Alejandro Cardoso <jcardogo@gmail.com>
2025-01-06 22:29:37 -0500

    Update use_SMTP.py

--- Automating_Real-World_Tasks_with_Python/email_lib/use_SMTP.py
+++ Automating_Real-World_Tasks_with_Python/email_lib/use_SMTP.py
@@ -1,3 +1,26 @@
+import os.path
+attachement_path = "/tmp/example.png"
+attachement_filename = os.path.basename(attachement_path)
+import mimetypes
+mime_type, _ = mimetypes.guess_type(attachement_path)
+mime_type, mime_subtype = mime_type.split("/", 1
+
+from email.message import EmailMessage
+message = EmailMessage()
+sender = "jcardogo@gmail.com"
+recipient = "jairoalejandrocardoso@gmail.com"
+
+message["From"] = sender
+message["To"] = recipient
+
+message["Subject"] = "Greetings from {} to {}!".format(sender, recipient)
+
+body = """Hey there!
+
+Im learning to send emails using python!"""
+
+message.set_content(body)
+
 import smtplib
 
 mail_server= smtplib.SMTP("smtp.gmail.com")
