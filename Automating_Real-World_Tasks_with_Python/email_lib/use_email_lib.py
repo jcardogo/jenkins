5456d73448f9ec3ac4a7b5a32f22bebf60c0fa5e
Alejandro Cardoso <jcardogo@gmail.com>
2025-01-06 22:07:56 -0500

    Update use_email_lib.py

--- Automating_Real-World_Tasks_with_Python/email_lib/use_email_lib.py
+++ Automating_Real-World_Tasks_with_Python/email_lib/use_email_lib.py
@@ -1,3 +1,13 @@
+import os.path
+attachement_path = "/tmp/example.png"
+attachement_filename = os.path.basename(attachement_path)
+import mimetypes
+mime_type, _ = mimetypes.guess_type(attachement_path)
+print(mime_type)
+mime_type, mime_subtype = mime_type.split("/", 1)
+print(mime_type)
+print(mime_subtype)
+
 from email.message import EmailMessage
 message = EmailMessage()
 print(message)
@@ -19,4 +29,11 @@ Im learning to send emails using python!"""
 
 message.set_content(body)
 
+print(message)
+
+with open(attachement_path, "rb") as ap:
+  message.add_attachement(ap.read(),
+                         maintype=mime_type,
+                         subtype=mime_subtype,
+                         filename=os.path.basename(attachement_path))
 print(message)
