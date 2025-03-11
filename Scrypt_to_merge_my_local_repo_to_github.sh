7fea34e3d47437cb673f119cf05933ea84050ca2
Alejandro CARDOSO <jcardogo@gmail.com>
2025-03-11 16:02:57 -0400

    improving seuciry by removing secrets on file

--- Scrypt_to_merge_my_local_repo_to_github.sh
+++ Scrypt_to_merge_my_local_repo_to_github.sh
@@ -5,17 +5,12 @@ git commit Basic_Python_Syntax_introduction.py -m "third file commited from ubun
 git remote set-url origin https://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@github.com/jcardogo/DevOps
 
 ####HOw to clone a repo with Personal Access Token
-git clone https://acardogo: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@github.com/jcardogo/DevOps.git
+git clone https://jcardogo:GITHUB_PAT@github.com/jcardogo/DevOps.git
 
 ###### GitHub lab
-git clone https://acardogo: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@github.com/jcardogo/it-cert-automation-practice.git # cloning the repo for lab
-
+git clone https://jcardogo:$GITHUB_PAT@github.com/jcardogo/it-cert-automation-practice.git # cloning the repo for lab
 git remote add upstream https://github.com/jcardogo/it-cert-automation-practice.git #to create an upstream on remote repo
-
 git branch improve-username-behavior
-
 git checkout improve-username-behavior
-
 cd ~/it-cert-automation-practice/Course3/Lab4
-
 git push origin improve-username-behavior
