452510d8c4196af9f4dd3b6af3fb0de0309a2cdf
Alejandro CARDOSO <jcardogo@gmail.com>
2025-03-11 15:09:00 -0400

    Checking if updating the PAT works from VSC

--- Git_and_github_course/intalling_git_rocky8.sh
+++ Git_and_github_course/intalling_git_rocky8.sh
@@ -2,5 +2,5 @@
 
 # Install Git on Rocky Linux 8
 sudo yum install git -y
-git config --global user.name "Rocky8_01"
-git config --global user.email "jcardogo@gmail.com"
+git config --global user.name "Rocky8_01" # Change to your name
+git config --global user.email "jcardogo@gmail.com" # Change to your email
